7e12184b96bedaccd046a1b6d3d458b67d9195c1
benjilap <benjilap2@yahoo.fr>
2018-12-06 12:20:45 +0100

    up player

---
+++ BG2_CardGame/Assets/Scripts/SceneGizmosScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SceneGizmosScript : MonoBehaviour {
+
+    public float radius=1;
+    public Vector3 offsetSphere;
+    public Vector3 offsetCube;
+    public Vector3 shapeCube;
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position+offsetSphere, radius);
+
+        Gizmos.DrawWireCube(transform.position + offsetCube, shapeCube);
+    }
+}
---
+++ BG2_CardGame/Assets/Scripts/SphereShapeCylinder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SphereShapeCylinder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ModifShape();
+	}
+
+    void ModifShape()
+    {
+        transform.localScale = new Vector3(transform.localScale.x, transform.parent.localScale.x, transform.localScale.z);
+
+    }
+
+}
